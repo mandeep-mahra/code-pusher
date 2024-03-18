1ef15807bc981fc7b3c3a1a4f36e1ec87e8f0aa5
mandeep-mahra <mandeepmehara1@gmail.com>
2024-03-19 02:35:42 +0530

    fix: landing page

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-  <App submit = {false}/>
+  <App submit = {true}/>
  
 );
 
